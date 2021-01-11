e3c85b923534f1550cf3d62ccd8794476c711017
JungTag <dbsdudxor121@naver.com>
2021-01-12 02:31:12 +0900

    Add crawling

--- Crawling/crawling.py
+++ Crawling/crawling.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import requests
 import time
+import re
 
 options = webdriver.ChromeOptions()
 # options.add_argument("headless")
@@ -9,7 +12,7 @@ driver = webdriver.Chrome('./chromedriver.exe')
 url = "https://www.diningcode.com/list.php?query=외대"
 driver.get(url)
 
-for i in range(2):
+for i in range(10):
     time.sleep(1)
     btn = driver.find_element_by_css_selector("span[class='more-btn']")
     btn.click()
@@ -18,23 +21,44 @@ for i in range(2):
 lis = driver.find_elements_by_css_selector("ul#div_list > li")
 for li in lis:
     if li.get_attribute("onmouseenter") != None:
-        li.click()
-        driver.switch_to_window(driver.window_handles[-1])
-        res_name = driver.find_element_by_class_name("tit").text
-        print(res_name)
-        time.sleep(5)
+        a = li.find_element_by_css_selector("a[class='blink']")
+        url = a.get_attribute("href")
+        print(url)
+        time.sleep(1)
+
+        res = requests.get(url)
+        time.sleep(0.1)
+        html = bs(res.text, 'html.parser')
+
+        title = html.select_one('div.tit-point > p.tit').text
+
+        try:
+            sign = html.select_one('div.btxt').text
+            sign = re.sub('외대', '', sign)
+            sign = re.sub('[^A-Za-z0-9가-힣,]', '', sign)
+            # split후 리스트에 담아주면 됨
+        except:
+             sign = ""
+        addr = html.select_one('li.locat').text
+        tel = html.select_one('li.tel').text
+        menu = []
+        price = []
+        try:
+            menu_info = html.select_one('div.menu-info')
+            menus = html.find_all("p", "l-txt Restaurant_MenuItem")
+            prices = html.find_all("p", "r-txt Restaurant_MenuPrice")
+            for m in menus:
+                m = m.text
+                m = re.sub('\n', '', m)
+                menu.append(m)
+            for p in prices:
+                price.append(p.text)
+            items = list(zip(menu, price))
+        except:
+            print("no menuInfo")
+        print(title, addr, tel, sign, sep='\n')
+        for item in items:
+            print(*item)
 
 driver.close()
 driver.quit()
-'''
-url = "https://www.diningcode.com/list.php?query=외대"
-
-response = req
-uests.get(url)
-html = bs(response.text, "html.parser")
-
-# lists = html.select("li[onmouseenter^='setIcon']")
-lists = html.findAll("li", {"onmouseenter": re.compile("^setIcon")})
-for list in lists:
-    print(list)
-'''
