3e393d7e4df72317ba4bf002757b7abe365b8648
JungTag <dbsdudxor121@naver.com>
2021-01-19 22:32:40 +0900

    add checkpoint

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ import uvicorn
 from sqlalchemy.orm import Session, sessionmaker
 import models, crud, schemas
 from database import SessionLocal, engine
+from routes import menu
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,19 +22,7 @@ def get_db():
     finally:
         db.close()
 
-# 전체 메뉴
-@app.get("/menus/", response_model=List[schemas.Menu])
-def all(db: Session = Depends(get_db)):
-    menus = crud.get_menus(db)
-    return menus
-
-# 추천에 의한 메뉴(카테고리, 종류, 가격대)
-@app.get("/menus/{category_pk}/{kind_pk}/{price_pk}", response_model=List[schemas.Menu])
-def recommend(category_pk: int, kind_pk:int, price_pk:int, db: Session = Depends(get_db)):
-    menus = crud.get_menus_by_select(db, category_pk=category_pk, kind_pk=kind_pk, price_pk=price_pk)
-    if menus is None:
-        raise HTTPException(status_code=404)
-    return menus
+app.include_router(menu.router, tags=["Menus"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
--- backend/routes/menu.py
+++ backend/routes/menu.py
@@ -1,3 +1,23 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import models, crud, schemas
+from database import get_db
+
+router = APIRouter(prefix='/menus')
+
+# 전체 메뉴
+@router.get("/", response_model=List[schemas.Menu])
+def all(db: Session = Depends(get_db)):
+    menus = crud.get_menus(db)
+    return menus
+
+# 추천에 의한 메뉴(카테고리, 종류, 가격대)
+@router.get("/{category_pk}/{kind_pk}/{price_pk}", response_model=List[schemas.Menu])
+def recommend(category_pk: int, kind_pk:int, price_pk:int, db: Session = Depends(get_db)):
+    menus = crud.get_menus_by_select(db, category_pk=category_pk, kind_pk=kind_pk, price_pk=price_pk)
+    if menus is None:
+        raise HTTPException(status_code=404)
+    return menus
