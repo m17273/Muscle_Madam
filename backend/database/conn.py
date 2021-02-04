448f14c01bfa805585841d81c8a0c44bf630e938
m17273 <17273@naver.com>
2021-02-04 23:21:46 +0900

    지현

--- backend/database/conn.py
+++ backend/database/conn.py
@@ -8,7 +8,7 @@ SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
 secrets = json.loads(open(SECRET_FILE).read())
 DB = secrets["DB"]
 
-DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8mb4"
 
 engine = create_engine(
     DB_URL, encoding = 'utf-8'
